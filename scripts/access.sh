523c72e8907d70dfe6c7c4abf8e3df189a428335
COUNTRik <COUNTRik@yandex.ru>
2020-09-10 17:26:07 +0300

    Update access.sh

--- scripts/access.sh
+++ scripts/access.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+alline=$(wc -l access-4560-644067.log | cut -d ' ' -f 1)
+numline=$(grep -n $(cat time) access-4560-644067.log | awk -F: '{print $1}' | head -n 1)
+echo $alline
+echo $numline
+line=$(($alline - $numline))
+echo $line
+# awk '{print $1,$4,$6,$9}' access-4560-644067.log > access.log
+# awk 'END{print $2}' access.log | cut -c 2- > time
+# cat time
+# awk '/(GET|POST)/{print $1}' access.log | sort -n | uniq -c | sort -n -r > get-post-ip.log
+# awk '/GET/{print $1}' access.log | sort -n | uniq -c | sort -n -r > post-ip.log
+# awk '! /(GET|POST)/{print $1}' access.log > ip-error.log
+# awk '/(GET|POST)/{print $4}' access.log | sort -n | uniq -c | sort -n -r > code.log
