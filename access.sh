799a7be4b2ca137e7b7ce39687a57f7bec52955b
COUNTRik <COUNTRik@yandex.ru>
2020-09-29 18:05:34 +0300

    Update access.sh

--- access.sh
+++ access.sh
@@ -3,8 +3,15 @@ alline=$(wc -l access-4560-644067.log | cut -d ' ' -f 1)
 numline=$(grep -n $(cat time) access-4560-644067.log | awk -F: '{print $1}' | head -n 1)
 line=$(($alline - $numline))
 
-# Сохраняем в отдельный файл все нужные нам поля лог файла, которые мы будем обрабатывать, для того чтобы не мешать записывать в основной файл логи 
+# Сохраняем в отдельный файл все нужные нам поля лог файла все те строки, которые идут после даты из файла , которые мы будем обрабатывать, для того чтобы не мешать записывать в основной файл лог
+awk '{print $1,$4,$6,$7,$9}' access-4560-644067.log > access.log
+echo "Count lines without marker time"
+wc -l access.log
+
 tail -n $line access-4560-644067.log | awk '{print $1,$4,$6,$7,$9}' > access.log
+echo "Count lines with marker time"
+wc -l access.log
+
 
 # Записываем в файл time последнюю дату последней обрабатываемой строки, с помощью которой мы сможем начать обработку нашего лога в следующий запуск (раскоментировать для включения)
 # awk 'END{print $2}' access.log | cut -c 2- > time
@@ -22,7 +29,7 @@ awk '/(GET|POST)/{print $1}' access.log | sort -n | uniq -c | sort -n -r | head 
 echo "-----------------------------------------------------------------------------------" >> weblog
 
 echo "The list of server resources with the most requests from the clients (top 10 req-res pairs)" >> weblog
-awk '{print $4}' access.log | sort -n | uniq -c | sort -n -r | head >> weblog
+awk '/(GET|POST)/{print $4}' access.log | sort -n | uniq -c | sort -n -r | head >> weblog
 echo "-----------------------------------------------------------------------------------" >> weblog
 
 echo "Total number of errors (status codes 4xx and 5xx, number-code pairs)" >> weblog
